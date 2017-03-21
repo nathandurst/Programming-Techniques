673406fdbd53f60c26cf671ccd1537afe953271f
Nathan Durst <nadurst0303@my.mwsu.edu>
2017-03-21 09:46:23 -0500

    add Goldbach's Conjecture problem

---
+++ Goldbach's Conjecture.cpp
@@ -0,0 +1,67 @@
+/*////////////////////////////////////////////////
+
+Nathan Durst	:	nadurst0303
+Project 17 - Goldbach's Conjecture - UVa #543
+Programming Techiques - Simpson
+March 1, 2017
+
+*////////////////////////////////////////////////
+
+#include <iostream>
+#include <bitset>
+#include <vector>
+using namespace std;
+
+typedef::_Longlong ll;
+ll _sieve_size;
+bitset<10000010> bs;
+vector<int> primes;
+
+void sieve(ll upperbound)
+{
+	_sieve_size = upperbound + 1;
+	bs.set();
+	bs[0] = bs[1] = 0;
+	for (ll i = 2; i <= _sieve_size; i++)
+	{
+		if (bs[i])
+		{
+			for (ll j = i*i; j <= _sieve_size; j += i)
+				bs[j] = 0;
+			primes.push_back((int)i);
+		}
+	}
+}
+
+bool isPrime(int n)
+{
+	for (int i = 0; i < primes.size(); i++)
+	{
+		if (n == primes[i])
+			return true;
+	}
+	return false;
+}
+
+int main()
+{
+	sieve(10000000);
+	int n, a, b;
+	cin >> n;
+	while (n != 0)
+	{
+		for (int i = 1; i < primes.size(); i++)
+		{
+			a = primes[i];
+			b = n - a;
+			if (isPrime(b))
+			{
+				cout << n << " = " << a << " + " << b << '\n';
+				break;
+			}
+		}
+		cin >> n;
+	}
+
+	return 0;
+}
