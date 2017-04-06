699894d7bf36d1b6a27929652b45700f3f687288
Nathan Durst <nadurst0303@my.mwsu.edu>
2017-04-06 11:04:36 -0500

    modified Circumference.cpp

--- Circumference.cpp
+++ Circumference.cpp
@@ -8,10 +8,10 @@ April 3, 2017
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-#define PI 3.141592653589793239
+#define PI 3.141592653
 using namespace std;
 
-float dist(double x1, double y1, double x2, double y2)
+double dist(double x1, double y1, double x2, double y2)
 {
 	return (sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
 }
@@ -20,19 +20,18 @@ int main()
 {
 	double x1, y1, x2, y2, x3, y3;
 	double a, b, c;
-	double perimeter, semi, area, radius;
+	double s, area, radius;
 	while (cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3)
 	{
 		a = dist(x1, y1, x2, y2);
 		b = dist(x2, y2, x3, y3);
 		c = dist(x3, y3, x1, y1);
 
-		perimeter = a + b + c;
-		semi = perimeter / 2;
-		area = sqrt(semi * (semi - a) * (semi - b) * (semi - c));
+		//perimeter = a + b + c;
+		s = (a + b + c) / 2;
+		area = sqrt(s * (s - a) * (s - b) * (s - c));
 		radius = (a*b*c) / (4 * area);
-		cout << fixed << setprecision(2) << 2 * PI*radius << '\n';
-
+		cout << fixed << setprecision(2) << 2 * PI * radius << '\n';
 	}
 
 	return 0;
